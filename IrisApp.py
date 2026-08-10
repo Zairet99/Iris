1b7fd92fbbeb4957156a02d6791faf9dd4364126
Zairet99 <zperezv001@alumno.uaemex.mx>
2026-08-09 23:16:37 -0600

    Refactor user input and update model to RandomForest

--- IrisApp.py
+++ IrisApp.py
@@ -9,17 +9,17 @@ st.write(''' # Clasificador de Iris ''')
 st.image("iris.webp", caption="Tipos de flor Iris.")
 
 def user_input_features():
-    petal_length = st.number_input('Longitud del pétalo (cm):', min_value=1, max_value=7, value=3, step=1)
-    petal_width = st.number_input('Ancho del pétalo (cm):', min_value=0, max_value=2, value=1, step=1)
-    sepal_length = st.number_input('Longitud del sépalo (cm):', min_value=4, max_value=8, value=5, step=1)
-    sepal_width = st.number_input('Ancho del sépalo (cm):', min_value=2, max_value=4, value=3, step=1)
+    petal_length = st.slider('Longitud del pétalo:', 1, 7, 3)
+    petal_width = st.slider('Ancho del pétalo:', 0, 2, 1)
+    sepal_length = st.slider('Longitud del sépalo:', 4, 8, 5)
+    sepal_width = st.slider('Ancho del sépalo:', 2, 4, 3)
     
     return pd.DataFrame([[sepal_length, sepal_width, petal_length, petal_width]], 
                        columns=['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)'])
 
 df_input = user_input_features()
 
-# Cargar datos Iris 
+# Cargar datos
 iris = load_iris()
 df = pd.DataFrame(iris.data, columns=iris.feature_names)
 df['species'] = iris.target
@@ -33,8 +33,8 @@ le = LabelEncoder()
 y_encoded = le.fit_transform(y)
 X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.3, random_state=42)
 
-# Modelo
-model = DecisionTreeClassifier(criterion="gini", max_depth=3, random_state=42)
+# Modelo balanceado
+model = RandomForestClassifier(n_estimators=150, max_depth=4, max_features='sqrt', random_state=42)
 model.fit(X_train, y_train)
 
 # Predicción
@@ -42,5 +42,4 @@ prediction = model.predict(df_input)[0]
 species = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
 
 st.subheader('Predicción')
-st.write(f'**La flor es:** {species[prediction]}')
-
+st.write(f'**{species[prediction]}**')
