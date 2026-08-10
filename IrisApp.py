4f06258da2b71c0466d328b6d87f4a8b1ae304b7
Zairet99 <zperezv001@alumno.uaemex.mx>
2026-08-09 23:18:38 -0600

    Replace DecisionTreeClassifier with RandomForestClassifier

--- IrisApp.py
+++ IrisApp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
